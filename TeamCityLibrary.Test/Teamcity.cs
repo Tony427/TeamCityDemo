07ec0e10626b1cd515534d214ef3b75d1527d49c
TonyTsai <tony@unifiedpractice.com>
2019-09-18 11:15:06 +0800

    first commit

---
+++ TeamCityLibrary.Test/Teamcity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamCityLibrary.Test
+{
+    public static class TeamCity
+    {
+        static string Test(string input)
+        {
+            return string.IsNullOrEmpty(input) ? "no inupt" : $"Echo : {input}";
+        }
+    }
+}
