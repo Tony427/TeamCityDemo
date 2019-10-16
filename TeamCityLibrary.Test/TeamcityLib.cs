38caeb251d94bffd324a6463e4de414b0bac8cd6
TonyTsai <tony@unifiedpractice.com>
2019-10-16 19:13:13 +0800

    1.0.9

---
+++ TeamCityLibrary.Test/TeamcityLib.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TeamCityLibrary.Test
+{
+    public  class TeamCityLib
+    {
+        public string Echo(string input)
+        {
+            return string.IsNullOrEmpty(input) ? "no inupt" : $"Echo : {input}";
+        }
+        // update
+
+        public string A()
+        {
+            return "A";
+        }
+
+        public string B()
+        {
+            return "B";
+        }
+
+        public string C()
+        {
+            return "C";
+        }
+    }
+}
